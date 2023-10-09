f3b4fd5f0cc8cb1e91d0ac56936cc5965398ee88
prachi-2201 <145090403+prachi-2201@users.noreply.github.com>
2023-10-09 09:27:26 +0530

    Add files via upload

---
+++ odd_even_for.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main()
+{
+	int i;
+	printf("\n EVEN : ");
+	for(i=1;i<=10;i++)
+	{
+		if(i%2==0)
+		{
+			printf("\n %d",i);
+		}
+	}
+	printf("\n\n ODD :");
+	for(i=1;i<=10;i++)
+	{
+		if(i%2==1)
+		{
+			printf("\n %d",i);
+		}
+	}
+}
